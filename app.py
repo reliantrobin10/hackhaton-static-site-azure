cb4c0a23174ebc02d0001d07fb1560187d88383e
reliantrobin10 <105748797+reliantrobin10@users.noreply.github.com>
2024-04-06 14:01:39 +1100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/subject')
+def subject():
+    return render_template('subject.html')
+
+@app.route('/AI_assist.html')
+def ai_assist():
+    return render_template('AI_assist.html')
+
+@app.route('/quiz.html')
+def quiz():
+    return render_template('quiz.html')
+
+@app.route('/assignment.html')
+def assignment():
+    return render_template('assignment.html')
+
+@app.route('/teaching.html')
+def teaching():
+    return render_template('teaching.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
